fc4d573fa9587a1b39bfede1acd32e60333f2713
Andrea Cifola <cifola.andrea@gmail.com>
2018-12-07 09:26:33 +0100

    Refactor main class

--- src/main/java/com/example/demo/OrderServiceMain.java
+++ src/main/java/com/example/demo/OrderServiceMain.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderServiceApplication {
+public class OrderServiceMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+		SpringApplication.run(OrderServiceMain.class, args);
 	}
 }
